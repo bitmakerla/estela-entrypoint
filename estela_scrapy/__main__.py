feb826428f5f8af7f4a9f3ab1dab895d0ee0549b
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2022-08-05 02:38:35 -0500

    Test

--- estela_scrapy/__main__.py
+++ estela_scrapy/__main__.py
@@ -22,6 +22,7 @@ def run_scrapy(argv, settings, describe):
 
         crawler_process = CrawlerProcess(settings)
         crawler = crawler_process.create_crawler(argv[2])
+        crawler.crawl()
         crawler_process.join()
 
         print("STATS", crawler.stats.get_stats())
