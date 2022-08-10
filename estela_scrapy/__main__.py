9e62ae6df487a1f160c1fb845dd97c25309b3a43
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2022-08-10 00:08:13 -0500

    Test

--- estela_scrapy/__main__.py
+++ estela_scrapy/__main__.py
@@ -2,10 +2,47 @@ import os
 import signal
 import sys
 import logging
+from scrapy.crawler import CrawlerProcess
 
 
-def print_whatevs():
-    print("MADE IT HERE")
+class MyCP(CrawlerProcess):
+    def print_whatevs():
+        print("MADE IT HERE")
+
+    def start(self, stop_after_crawl=True, install_signal_handlers=True):
+        """
+        This method starts a :mod:`~twisted.internet.reactor`, adjusts its pool
+        size to :setting:`REACTOR_THREADPOOL_MAXSIZE`, and installs a DNS cache
+        based on :setting:`DNSCACHE_ENABLED` and :setting:`DNSCACHE_SIZE`.
+
+        If ``stop_after_crawl`` is True, the reactor will be stopped after all
+        crawlers have finished, using :meth:`join`.
+
+        :param bool stop_after_crawl: stop or not the reactor when all
+            crawlers have finished
+
+        :param bool install_signal_handlers: whether to install the shutdown
+            handlers (default: True)
+        """
+        from twisted.internet import reactor
+
+        if stop_after_crawl:
+            d = self.join()
+            # Don't start the reactor if the deferreds are already fired
+            if d.called:
+                return
+            d.addBoth(self._stop_reactor)
+
+        if install_signal_handlers:
+            install_shutdown_handlers(self._signal_shutdown)
+        resolver_class = load_object(self.settings["DNS_RESOLVER"])
+        resolver = create_instance(resolver_class, self.settings, self, reactor=reactor)
+        resolver.install_on_reactor()
+        tp = reactor.getThreadPool()
+        tp.adjustPoolsize(maxthreads=self.settings.getint("REACTOR_THREADPOOL_MAXSIZE"))
+        reactor.addSystemEventTrigger("before", "shutdown", self.print_whatevs)
+        reactor.addSystemEventTrigger("before", "shutdown", self.stop)
+        reactor.run(installSignalHandlers=False)  # blocking call
 
 
 def run_scrapy(argv, settings, describe):
@@ -18,6 +55,7 @@ def run_scrapy(argv, settings, describe):
     else:
         from scrapy import spiderloader
         from scrapy.crawler import Crawler, CrawlerProcess
+        from twisted.internet import reactor
 
         spider_loader = spiderloader.SpiderLoader.from_settings(settings)
         print(f"SCRAPING SPIDER {argv[2]}")
@@ -27,17 +65,20 @@ def run_scrapy(argv, settings, describe):
         print(f"SPIDER {spider}")
 
         crawler_process = CrawlerProcess(settings)
-        crawler = crawler_process.create_crawler(spider_class)
-        crawler.signals.connect(print_whatevs, signal.SIGUSR1)
-        crawler.crawl()
-        crawler_process.join()
+        #  crawler = crawler_process.create_crawler(spider_class)
+        #  crawler.signals.connect(print_whatevs, signal.SIGUSR1)
+        #  crawler.crawl()
+        #  d = crawler_process.join()
+        #  if not d.called:
+        #  d.addBoth(crawler_process._stop_reactor)
+
         #  crawler_process.crawl()
-        #  crawler_process.crawl(argv[2])
+        crawler_process.crawl(argv[2])
         #  crawler_process.signals.connect(print_whatevs, signal.SIGUSR1)
-        #  crawler_process.start()
+        crawler_process.start()
 
         print(f"CRAWLERS {crawler_process.crawlers}")
-        #  print("STATS", crawler_process.crawlers[0].stats.get_stats())
+        print("STATS", crawler_process.crawlers[0].stats.get_stats())
 
 
 def run_code(args, commands_module=None, describe=False):
