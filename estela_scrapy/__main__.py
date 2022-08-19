5bb92e5e5bc908a28bd8ec06f5b81b95a79ca69d
Alonso-Valdivia <34718875+Adriana618@users.noreply.github.com>
2022-08-19 14:06:36 -0500

    BITMAKER-1997: Add send logs to kafka from entrypoint (#17)
    * BITMAKER-1864: First draft
    
    * BITMAKER-1864: Initial draft
    
    * BITMAKER-1997: Add send logs to kafka from entrypoint
    
    * BITMAKER-1997: Linted and formated
    
    Co-authored-by: Alonso Valdivia <alonso.valdivia.quispe@ucsp.edu.pe>

--- estela_scrapy/__main__.py
+++ estela_scrapy/__main__.py
@@ -11,7 +11,7 @@ def run_scrapy(argv, settings):
     execute(settings=settings)
 
 
-def run_code(args, commands_module=None):
+def run_code(args, log_handler=None, commands_module=None):
     try:
         from estela_scrapy.settings import populate_settings
 
@@ -19,6 +19,8 @@ def run_code(args, commands_module=None):
         settings = populate_settings()
         if commands_module:
             settings.set("COMMANDS_MODULE", commands_module, priority="cmdline")
+        if log_handler is not None:
+            log_handler.setLevel(settings["LOG_LEVEL"])
     except Exception:
         logging.exception("Settings initialization failed")
         raise
@@ -35,7 +37,10 @@ def describe_project():
 
     setup_scrapy_conf()
 
-    run_code(["scrapy", "describe_project"] + sys.argv[1:], "estela_scrapy.commands")
+    run_code(
+        ["scrapy", "describe_project"] + sys.argv[1:],
+        commands_module="estela_scrapy.commands",
+    )
 
 
 def setup_and_launch():
@@ -48,12 +53,15 @@ def setup_and_launch():
 
         os.environ.update(env)
         setup_scrapy_conf()
-        # init logger [!] missing
+
+        from estela_scrapy.log import init_logging
+
+        loghdlr = init_logging()
     except:
         logging.exception("Environment variables were not defined properly")
         raise
 
-    run_code(args)
+    run_code(args, loghdlr)
 
 
 def main():
---
+++ estela_scrapy/log.py
@@ -0,0 +1,149 @@
+import logging
+import sys
+import os
+import warnings
+import time
+
+from twisted.python import log as txlog
+from scrapy import __version__
+from estela_scrapy.utils import to_standar_str
+from estela_scrapy.producer import connect_kafka_producer, on_kafka_send_error
+
+_stderr = sys.stderr
+
+
+def _logfn(level, message, parent="none"):
+    producer = connect_kafka_producer()
+    data = {
+        "jid": os.getenv("ESTELA_SPIDER_JOB"),
+        "payload": {"log": str(message), "datetime": float(time.time())},
+    }
+    response = producer.send("job_logs", value=data)
+
+
+def init_logging():
+    # General python logging
+    root = logging.getLogger()
+    root.setLevel(
+        logging.NOTSET
+    )  # NOSET Make processing all messages if is set in root
+
+    hdlr = LogHandler()
+    hdlr.setLevel(logging.INFO)
+    hdlr.setFormatter(logging.Formatter("[%(name)s] %(message)s"))
+    root.addHandler(hdlr)
+
+    # Silence commonly used noisy libraries
+    nh = logging.NullHandler()
+    for ln in ("boto", "requests", "kafka.conn"):
+        lg = logging.getLogger(ln)
+        lg.propagate = 0
+        lg.addHandler(nh)
+
+    # Redirect standard output and error
+    sys.stdout = StdoutLogger(False, "utf-8")
+    sys.stderr = StdoutLogger(True, "utf-8")
+
+    # Twisted specifics (includes Scrapy)
+    obs = LogObserver(hdlr)
+    _oldshowwarning = warnings.showwarning
+    txlog.startLoggingWithObserver(obs.emit, setStdout=False)
+    warnings.showwarning = _oldshowwarning
+    return hdlr
+
+
+class LogHandler(logging.Handler):
+    """Python logging handler"""
+
+    def emit(self, record):
+        try:
+            message = self.format(record)
+            if message:
+                _logfn(message=message, level=record.levelno, parent="LogHandler")
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
+    def handleError(self, record):
+        cur = sys.stderr
+        try:
+            sys.stderr = _stderr
+            super(LogHandler, self).handleError(record)
+        finally:
+            sys.stderr = cur
+
+
+class LogObserver(object):
+    """Twisted log observer"""
+
+    def __init__(self, loghdlr):
+        self._hs_loghdlr = loghdlr
+
+    def emit(self, ev):
+        logitem = self._get_log_item(ev)
+        if logitem:
+            _logfn(**logitem, parent="LogObserver")
+
+    def _get_log_item(self, ev):
+        """Get logs from scrapy in his level or Info level in other case"""
+        if ev["system"] == "scrapy":
+            level = ev["logLevel"]
+        else:
+            if ev["isError"]:
+                level = logging.ERROR
+            else:
+                level = logging.INFO
+
+        # Ignore levels
+        if level < self._hs_loghdlr.level:
+            return
+
+        msg = ev.get("message")
+        if msg:
+            msg = to_standar_str(msg[0])
+
+        failure = ev.get("failure", None)
+        if failure:
+            msg = failure.getTraceback()
+
+        why = ev.get("why", None)
+        if why:
+            msg = "{}\n{}".format(why, msg)
+
+        fmt = ev.get("format")
+        if fmt:
+            try:
+                msg = fmt.format(ev)
+            except:
+                msg = "FAILED TO APPLY FORMAT: fmt={} ev={}".format(repr(fmt), repr(ev))
+                level = logging.ERROR
+
+        msg = msg.replace("\n", "\n\t")
+        return {"message": msg, "level": level}
+
+
+class StdoutLogger(txlog.StdioOnnaStick):
+    """Catch logs from sterr and stdout"""
+
+    def __init__(self, isError=False, encoding=None, loglevel=logging.INFO):
+        txlog.StdioOnnaStick.__init__(self, isError, encoding)
+        self.prefix = "[stderr] " if isError else "[stdout] "
+        self.loglevel = loglevel
+
+    def _logprefixed(self, msg):
+        _logfn(message=self.prefix + msg, level=self.loglevel, parent="StdoutLogger")
+
+    def write(self, data):
+        data = to_standar_str(data, self.encoding)
+
+        d = (self.buf + data).split("\n")
+        self.buf = d[-1]
+        messages = d[0:-1]
+        for message in messages:
+            self._logprefixed(message)
+
+    def writelines(self, lines):
+        for line in lines:
+            line = to_standar_str(line, self.encoding)
+            self._logprefixed(line)
